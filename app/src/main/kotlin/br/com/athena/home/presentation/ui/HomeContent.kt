6b106b2d55786c0a8a049e9cb36888ca5fb78c5c
Guilherme <siedg104@gmail.com>
2023-12-11 16:41:39 -0300

    Added coroutine dependency

--- app/src/main/kotlin/br/com/athena/home/presentation/ui/HomeContent.kt
+++ app/src/main/kotlin/br/com/athena/home/presentation/ui/HomeContent.kt
@@ -3,13 +3,23 @@ package br.com.athena.home.presentation.ui
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.DrawerValue
+import androidx.compose.material.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import br.com.athena.components.navigationdrawer.AthenaNavigationDrawer
 import br.com.athena.components.texts.AthenaText_16Bold
+import kotlinx.coroutines.launch
 
 @Composable
 fun HomeContent() {
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+    val openDrawer = { scope.launch { drawerState.open() } }
+
+    AthenaNavigationDrawer(icon = 0, text = "", onDestinationClicked = {})
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
